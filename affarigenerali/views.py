0ae6bd37e6a01188a725e886cf0d9fc87a6ba23b
Simone Dalla <simodalla@gmail.com>
2014-11-15 11:15:56 +0100

    fix template dir for RiepiloghiPresenzeFormView view

--- affarigenerali/views.py
+++ affarigenerali/views.py
@@ -10,7 +10,7 @@ from .forms import FilterRiepiloghiPresenze
 
 
 class RiepiloghiPresenzeFormView(FormView):
-    template_name = "organigrammi/riepilogo_presenze.html"
+    template_name = "affarigenerali/riepilogo_presenze.html"
     form_class = FilterRiepiloghiPresenze
 
     def get_request_data(self):
