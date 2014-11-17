5c639090d742b116375ac2181709ac07bc34b5f5
Simone Dalla <simodalla@gmail.com>
2014-11-17 12:31:05 +0100

    add applications configuration, view https://docs.djangoproject.com/en/1.7/ref/applications/

---
+++ pympa_affarigenerali/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+from django.apps import AppConfig
+
+
+class PympaAffariGeneraliConfig(AppConfig):
+    name = 'pympa_affarigenerali'
+    verbose_name = 'Affari Generali'
